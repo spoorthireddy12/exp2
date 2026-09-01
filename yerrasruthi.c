0016f8f852e051cee41f9c43a5fff730c3b2aa39
spoorthireddy12 <spooorthireddymothe>
2026-09-01 10:56:12 +0530

    newfilesadded

---
+++ yerrasruthi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+int arr[100];
+int*
+printf("Enter number of elements:");
+scanf("%d",&n);
+printf("Enter number %d integers:\n",n);
+for(i=0,i<n;i++);
+{
+scanf("%d",(ptr+i));//using pointers to display");
+}
+return 0;
+}
+
